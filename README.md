92305b9f3c1a202f85d3437e2a5201a993c6dfb4
Soukoulis Tasos <sh4rk1zm4st3rc0der@gmail.com>
2018-03-16 04:56:19 +0200

    First commit, readme + main

---
+++ README.md
@@ -0,0 +1,21 @@
+# Haskell Toys
+
+A playground with small scripts created by learning haskell (Besides the requirement's and the code provided I'm not able to copy any code.)
+
+## Toys Lists
+Resource  | Associated file
+----------|----------------
+01. CIS 194 Homework 1 | - [ ] &nbsp; ➟  src/a_credit_card_validation.hs
+02. CIS 194 Homework 1, ex5 | - [ ] &nbsp; ➟ src/b_towers_of_hanoi.hs
+03. CIS 194 Homework 2 | - [ ] &nbsp; ➟ src/c_log_file_parsing.hs
+04. CIS 194 Homework 3 | - [ ] &nbsp; ➟ src/d_code_golf.hs
+05. CIS 194 Homework 4 | - [ ] &nbsp; ➟ src/e_wholemeal_programming.hs
+06. PIH Ch 5.5 uppercase letters| - [ ] &nbsp; ➟ src/f_caesar_cipher.hs
+07. PIH Ch 7.6, + exercise 7 | - [ ] &nbsp; ➟ src/g_binary_string_transmitter.hs
+08. PIH Ch 7.7 | - [ ] &nbsp; ➟ src/h_voting_algorithms.hs
+09. PIH Ch 8.6 + exercise 8 | - [ ] &nbsp; ➟ src/i_tautology_checker.hs
+10. PIH Ch 8.7 + exercise 9 | - [ ] &nbsp; ➟ src/j_abstract_machine.hs
+11. PIH Ch 9 | - [ ] &nbsp; ➟ src/k_countdown_problem.hs
+
+> PIH = Programming in Python by Graham Hutton
+> CIS 194 - 2013 Edition = http://www.seas.upenn.edu/~cis194/spring13
---
+++ app/Main.hs
@@ -0,0 +1,24 @@
+module Main where
+
+import Lib
+
+-- A playground with small scripts created by learning haskell (Besides the requirement's and the code provided I'm not able to copy any code.)
+-- PIH = Programming in Python by Graham Hutton
+-- CIS 194 - 2013 Edition = http://www.seas.upenn.edu/~cis194/spring13
+
+{-| Toys Lists
+01. CIS 194 Homework 1                => ✕ src/a_credit_card_validation.hs
+02. CIS 194 Homework 1, ex5           => ✕ src/b_towers_of_hanoi.hs
+03. CIS 194 Homework 2                => ✕ src/c_log_file_parsing.hs
+04. CIS 194 Homework 3                => ✕ src/d_code_golf.hs
+05. CIS 194 Homework 4                => ✕ src/e_wholemeal_programming.hs
+06. PIH Ch 5.5 uppercase letters      => ✕ src/f_caesar_cipher.hs
+07. PIH Ch 7.6, + exercise 7          => ✕ src/g_binary_string_transmitter.hs
+08. PIH Ch 7.7                        => ✕ src/h_voting_algorithms.hs
+09. PIH Ch 8.6 + exercise 8           => ✕ src/i_tautology_checker.hs
+10. PIH Ch 8.7 + exercise 9           => ✕ src/j_abstract_machine.hs
+11. PIH Ch 9                          => ✕ src/k_countdown_problem.hs
+|-}
+
+main :: IO ()
+main = someFunc
